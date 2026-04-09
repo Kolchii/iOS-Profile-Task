5b5bca20222a8b8936b40128d1b24e9187a3f363
Kolchi <kolchiibrahim@icloud.com>
2026-04-09 23:57:47 +0400

    Add initial SwiftUI iOS app Task project

---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Ibrahim Kolchi on 09.04.26.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
